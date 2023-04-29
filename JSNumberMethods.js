e6fea8c869762a901c3dc0fd91a72badd0ff1b73
Sandip <srai68141@gmail.com>
2023-04-29 22:29:22 +0545

    baishak16gatte, ajja samma javascript pani sakkena

---
+++ JSNumberMethods.js
@@ -0,0 +1,9 @@
+// JS Number Method
+// these number methods can be used on all javascript numbers
+
+Method:              // Description
+toString()              //return Number as a string
+toExponential()         // Return number written in exponential notation
+toFixed()               // returns a number written with a number of decimals
+toPrecision()           // Return a number written with a specific length
+Valueof()               // Returns a number as a number
---
+++ JSSorting Arrays.js
@@ -0,0 +1,8 @@
+// the sort() method sorts and array alphabetically:
+
+fruits.sort();
+
+
+// reversing an array
+
+fruits.reverse();
